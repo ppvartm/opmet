4f4ddd1d234e358a6e9b6ba81110cd5f5290c184
ppvartm <artm.ppv28@gmail.com>
2023-08-13 08:20:06 +0300

    Добавлен файл how to use

--- L_R_2/file_helper.cpp
+++ L_R_2/file_helper.cpp
@@ -2,15 +2,15 @@
 file_helper::file_helper(void* name_foo1,void* name_foo2)
 {
 	if (name_foo2 == name_foo1) {
-		write_level_lines.open("C:/Users/Артем/Desktop/level_lines_for_kvad.txt");
-		write_points.open("C:/Users/Артем/Desktop/points_for_kvad.txt");
-		write_simplex.open("C:/Users/Артем/Desktop/simplex_for_kvad.txt");
+		write_level_lines.open("pic/level_lines_for_kvad.txt");
+		write_points.open("pic/points_for_kvad.txt");
+		write_simplex.open("pic/simplex_for_kvad.txt");
 	}
 	else
 	{
-		write_level_lines.open("C:/Users/Артем/Desktop/level_lines_for_roz.txt");
-		write_points.open("C:/Users/Артем/Desktop/points_for_roz.txt");
-		write_simplex.open("C:/Users/Артем/Desktop/simplex_for_roz.txt");
+		write_level_lines.open("pic/level_lines_for_roz.txt");
+		write_points.open("pic/points_for_roz.txt");
+		write_simplex.open("pic/simplex_for_roz.txt");
 	}
 }
 void file_helper::add_data(std::pair<double, double> x, double y)
